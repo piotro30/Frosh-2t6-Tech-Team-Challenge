0962ad40d7ca45f77e0614aff0e738f487d7261f
piotro30 <ryan.piotrowski@mail.utoronto.ca>
2026-05-21 18:17:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,115 @@
 // Import the schedule data from the JSON file — you can use it like a regular JS array
 import scheduleData from './schedule_data.json';
+import './App.css';
+import uoftlogo from './assets/uoftlogo.png';
 
-// TODO: Build your Frosh Week Schedule page here
+
+
+function load_page(){
+
+  var frosh_color = "rgb(100,40,130)"
+  var dark_gray = "rgb(50,50,50)"
+
+
+  document.body.style.backgroundColor = dark_gray;
+
+}
+
+function formatTime(timeStr) {
+  if (!timeStr || timeStr.trim() === "") return "";
+
+  // remove "a1/p1" to replace with am/pm later
+  let cleaned = timeStr
+  .replace("a1/p1", "")
+  .trim();
+
+
+  // Otherwise try parsing 24-hour or messy formats
+  const [hours, minutes] = cleaned.split(":").map(Number);
+
+  if (isNaN(hours)) return cleaned;
+
+  const period = hours >= 12 ? "PM" : "AM";
+  const hour12 = ((hours + 11) % 12) + 1;
+  // Converts 24 hour time into 12 hr time
+
+  // If already in "7:00 PM" format, just return it
+  if (cleaned.includes("AM") || cleaned.includes("PM")) {
+    return cleaned;
+  }
+
+
+  return `${hour12}:${minutes?.toString().padStart(2, "0") || "00"} ${period}`;
+}
 
 function App() {
+
+  const days = Object.keys(scheduleData);
+
   return (
     <div>
-      <h1>scheduleData</h1>
-      {/*feel free to remove this*/}
+
+    <img
+      src={uoftlogo}
+
+      className="corner-image"
+    />
+
+
+    <h2 className="title">F!rosh Week Schedule</h2>
+
+    <div className="schedule-grid">
+
+    {days.map((day) => (
+      <div key={day} className="day-column">
+
+      <h3 className="day-title">{day}</h3>
+
+      {scheduleData[day].map((event, i) => (
+        <div
+        key={i}
+        className={`event-box ${event.Color} tooltip`}
+        >
+        <div className="event-name">
+        {event.Link ? (
+          <a
+          href={event.Link}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="event-link"
+          >
+          {event["Event Name"]}
+          </a>
+        ) : (
+          event["Event Name"]
+        )}
+        </div>
+
+        {event["Event Description"] && event["Event Description"].trim() !== "" &&(
+        <div className="tooltip-text">
+        {event["Event Description"]}
+        </div>)}
+
+
+
+        {event["Start Time"] && event["Start Time"].trim() && (
+          <div className="event-time">
+          {formatTime(event["Start Time"])} – {formatTime(event["End Time"])}
+          </div>
+        )}
+
+        </div>
+      ))}
+
+      </div>
+    ))}
+
+    </div>
+
     </div>
   );
 }
+
+load_page()
 
 export default App;
